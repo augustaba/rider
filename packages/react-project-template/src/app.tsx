213759f3302d1d2725e9a5d4f079faf1c3f850ac
liuhui <liuhui_0410@youzan.com>
2022-03-25 16:11:15 +0800

    chore: delete template project

--- packages/react-project-template/src/app.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => {
-  return <h1>Hello August </h1>;
-};
-export default App;
--- packages/react-project-template/src/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import App from './app';
-
-const Index: React.FC = () => <App />;
-export default Index;
